d3b632d72e4c8cc68e24e5e65e4a4a1e81c40815
WajihulHassan944 <82878378+WajihulHassan944@users.noreply.github.com>
2026-02-02 17:58:36 +0500

    updated code

--- constants/models.ts
+++
@@ -1,7 +0,0 @@
-export const modelData = [
-  { type: "Commission Only", fee: "€0/Month", comm: "15% per order", best: "New Restaurants, Variable Volume", isHighlight: true },
-  { type: "Subscription (Basic)", fee: "€99/month", comm: "0%", best: "Low-Volume Restaurants", isFeeHighlight: true },
-  { type: "Subscription (Premium)", fee: "€199/month", comm: "0%", best: "Medium-Volume Restaurants", isFeeHighlight: true },
-  { type: "Subscription (Enterprise)", fee: "€499/month", comm: "0%", best: "High-Volume, Multi-Location Chains", isFeeHighlight: true },
-  { type: "Hybrid Model", fee: "€49/month", comm: "8% per order", best: "Balanced Risk, Predictable Revenue", isFullHighlight: true },
-];
